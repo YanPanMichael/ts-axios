ac2255235e939dc2df582119a96fefa039a09380
YanPanMichael <yanpancsu@icloud.com>
2019-12-31 00:32:10 +0800

    add utils file

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -3,6 +3,18 @@ export function buildURL(url:string, params?:any): string {
   const parts: string[] = [];
   Object.keys(params).forEach((key) => {
     let val = params[key];
-    
+    if(val === null || typeof val === 'undefined') {
+      return
+    }
+    let values = []
+    if(Array.isArray(val)) {
+      values = val;
+      key += '[]'
+    } else {
+      values = [val]
+    }
+    values.forEach(val => {
+      
+    });
   })
 }
