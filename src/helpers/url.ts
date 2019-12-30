41f14d20649900159c74c0a388d1c191c0e04689
YanPanMichael <yanpancsu@icloud.com>
2019-12-30 23:58:40 +0800

    add url file

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,3 +1,8 @@
 export function buildURL(url:string, params?:any): string {
-  return 
+  if(!params) return url;
+  const parts: string[] = [];
+  Object.keys(params).forEach((key) => {
+    let val = params[key];
+    
+  })
 }
