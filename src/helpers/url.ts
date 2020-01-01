1867e5031b4095ec0db69e7d36eb2bb046ae05a1
YanPanMichael <yanpancsu@icloud.com>
2020-01-01 16:51:31 +0800

    add buildURL

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,20 +1,47 @@
-export function buildURL(url:string, params?:any): string {
-  if(!params) return url;
-  const parts: string[] = [];
-  Object.keys(params).forEach((key) => {
-    let val = params[key];
-    if(val === null || typeof val === 'undefined') {
+import { isDate, isObject } from './utils'
+
+function encode(val: string): string{
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/ig, ':')
+    .replace(/%24/ig, '$')
+    .replace(/%2C/ig, ',')
+    .replace(/%20/ig, '+')
+    .replace(/%5B/ig, '[')
+    .replace(/%5D/ig, ']')
+}
+
+export function buildURL(url: string, params?: any): string {
+  if (!params) return url
+  const parts: string[] = []
+  Object.keys(params).forEach(key => {
+    let val = params[key]
+    if (val === null || typeof val === 'undefined') {
       return
     }
     let values = []
-    if(Array.isArray(val)) {
-      values = val;
+    if (Array.isArray(val)) {
+      values = val
       key += '[]'
     } else {
       values = [val]
     }
     values.forEach(val => {
-      
-    });
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}-${encode(val)}`)
+    })
   })
+  let serializedParams = parts.join('&')
+  if(serializedParams) {
+    const marIndex = url.indexOf('#');
+    if(marIndex !== -1) {
+      url = url.slice(0, marIndex)
+    }
+    url += (url.indexOf('?') === -1?'?':'&' ) + serializedParams;
+  }
+  return url;
 }
--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,9 +1,13 @@
 const toString = Object.prototype.toString;
 
-export function isDate(val: any): boolean {
+export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
-export function isObject(val: any): boolean {
+export function isObject(val: any): val is Object {
   return val!==null && !Array.isArray(val) && typeof val === 'object'
+}
+
+export function isFunction(val: any): val is Function {
+  return typeof val === 'function'
 }
