7cd114ed83841c464bc9b0f34159f8d25573b1d4
YanPanMichael <yanpancsu@icloud.com>
2020-01-03 00:39:51 +0800

    save

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -70,17 +70,17 @@ axios({
   }
 })
 
-axios({
-  method: 'post',
-  url: '/base/post',
-  headers: {
-    'content-type': 'application/json;charset=utf-8'
-  },
-  data: {
-    a: 1,
-    b: 2
-  }
-})
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   headers: {
+//     'content-type': 'application/json;charset=utf-8'
+//   },
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// })
 
 // const arr = new Int32Array([21, 31])
 
